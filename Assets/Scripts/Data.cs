be982707732aec489b6b84d7a012cbf96b3a8ed9
manuel <manuel.martinbalmori@gmail.com>
2025-01-25 11:41:42 +0100

    updated basis for UI value check + basic button to gather oxygen. cleared code a bit.

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,14 +1,10 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Ce fichier a pour but de gerer les donnees ainsi que la gestion en arriere plan du jeu
+// TODO: Globalement celui la c'est le module manager, j'ai fais un peu de code cleaning,
+// mais si tu pouvais renommer la classe et les dependencies... j'ai pas trop touche au code pour pas faire de betises.
 public class Manager : MonoBehaviour
 {
-	public class Ressources
-	{
-		public int quantity = 0;
-	}
 	public class Module
 	{
 		public Vector2 position = new(0f, 0f);
@@ -17,24 +13,5 @@ public class Manager : MonoBehaviour
 	{
 		//liste des modules existants
 		public static List<Module> Modules_list = new();
-
-		//creation des ressources
-		public static Ressources Oxygen = new();
-		public static Ressources Food = new();
-		public static Ressources Energy = new();
-		public static Ressources Fortynium = new();
-		public static Ressources Wallet = new();
-		public static Ressources Humans = new();
-	}
-	private void Start()
-	{
-		// ressources de depart
-		Data.Oxygen.quantity = 10;
-		Data.Food.quantity = 10;
-		Data.Energy.quantity = 10;
-		Data.Humans.quantity = 10;
-	}
-	private void Update()
-	{
 	}
 }
